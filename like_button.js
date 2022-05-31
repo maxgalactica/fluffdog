d3edbc1c147609ec56d61569651688e4dbdcad17
Gideon Eyer <gideon.eyer@gmail.com>
2022-05-31 17:23:16 -0500

    Testing React button component

---
+++ like_button.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {liked:false};
+    }
+
+    render() {
+        if(this.state.liked) {
+            return 'hewwo OvO';
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({liked:true})}
+        )
+    }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(r(LikeButton));
