843d6cea3b022706d667735743f0843a941d7801
Gideon Eyer <gideon.eyer@gmail.com>
2022-05-31 17:49:35 -0500

    fixed misplaced R where e should be in JS function

--- like_button.js
+++ like_button.js
@@ -15,11 +15,12 @@ class LikeButton extends React.Component {
 
         return e(
             'button',
-            { onClick: () => this.setState({liked:true})}
-        )
+            { onClick: () => this.setState( {liked: true }) },
+            'Like'
+        );
     }
 }
 
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
-root.render(r(LikeButton));
+root.render(e(LikeButton));
